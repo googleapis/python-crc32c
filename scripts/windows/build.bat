644423236274799746410eae6e81b6b113932824
Chris Wilcox <crwilcox@google.com>
2019-11-25 16:14:49 -0800

    fix: add -G command back

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -14,18 +14,21 @@ py -3.7 -m pip install cmake
 
 @rem First, build libcrc32c
 set CRC32C_INSTALL_PREFIX=%KOKORO_ARTIFACTS_DIR%\bin\
+set CMAKE_GENERATOR=Visual Studio 15 2017
 pushd crc32c
 
 git submodule update --init --recursive
 
 mkdir build
 C:\Python37\Scripts\cmake ^
+    -G "%CMAKE_GENERATOR%" ^ 
     -DCRC32C_BUILD_TESTS=no ^
     -DCRC32C_BUILD_BENCHMARKS=no ^
     -DBUILD_SHARED_LIBS=yes ^
     -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes ^
     -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% ^
     .
+
 
 @REM cmake ^
 @REM     -G "%CMAKE_GENERATOR%" ^
