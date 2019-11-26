d137f9e8a9cb21c31dc3d8022f46e8f46def1f42
Chris Wilcox <crwilcox@google.com>
2019-11-26 11:05:05 -0800

    fix: bring back 32 bit builds

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -32,14 +32,15 @@ set CRC32C_INSTALL_PREFIX=%KOKORO_ARTIFACTS_DIR%\bin\
 
 echo %CRC32C_INSTALL_PREFIX%
 
-set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
 pushd crc32c
 
 git submodule update --init --recursive
 
 mkdir build
 
+@REM 64 Bit Builds.
 @REM removed -DCRC32C_BUILD_TESTS=no 
+set CMAKE_GENERATOR="Visual Studio 15 2017 Win64"
 C:\Python37\Scripts\cmake -G %CMAKE_GENERATOR% -DCRC32C_BUILD_BENCHMARKS=no -DBUILD_SHARED_LIBS=yes ^
 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% .
 
@@ -49,18 +50,25 @@ popd
 
 copy %CRC32C_INSTALL_PREFIX%bin\crc32c.dll .
 
-@REM @rem update python deps
-@REM py -3.5 -m pip install --upgrade pip setuptools wheel
-@REM py -3.6 -m pip install --upgrade pip setuptools wheel
-@REM py -3.7 -m pip install --upgrade pip setuptools wheel
+@rem update python deps and build wheels (requires CRC32C_INSTALL_PREFIX is set)
+@rem FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
+FOR %%V IN (3.5-64,3.6-64,3.7-64) DO (
+    py -%%V -m pip install --upgrade pip setuptools wheel
+    py -%%V -m pip wheel .
+)
 
-@REM @rem Build wheels (requires CRC32C_INSTALL_PREFIX is set)
-@REM py -3.5-64 -m pip wheel .
-@REM py -3.5-32 -m pip wheel .
-@REM py -3.6-64 -m pip wheel .
-@REM py -3.6-32 -m pip wheel .
-@REM py -3.7-64 -m pip wheel .
-@REM py -3.7-32 -m pip wheel .
+
+@REM 32 Bit Builds.
+@REM removed -DCRC32C_BUILD_TESTS=no 
+set CMAKE_GENERATOR="Visual Studio 15 2017"
+C:\Python37\Scripts\cmake -G %CMAKE_GENERATOR% -A Win32 -DCRC32C_BUILD_BENCHMARKS=no -DBUILD_SHARED_LIBS=yes ^
+-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% .
+
+C:\Python37\Scripts\cmake --build . --config RelWithDebInfo --target install
+dir %CRC32C_INSTALL_PREFIX% /b /s
+popd
+
+copy %CRC32C_INSTALL_PREFIX%bin\crc32c.dll .
 
 @rem update python deps and build wheels (requires CRC32C_INSTALL_PREFIX is set)
 @rem FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
@@ -68,4 +76,5 @@ FOR %%V IN (3.5-64,3.6-64,3.7-64) DO (
     py -%%V -m pip install --upgrade pip setuptools wheel
     py -%%V -m pip wheel .
 )
+
 
--- scripts/windows/test.bat
+++ scripts/windows/test.bat
@@ -15,7 +15,8 @@
 
 @rem update python deps and build wheels (requires CRC32C_INSTALL_PREFIX is set)
 @rem FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
-FOR %%V IN (3.5-64,3.6-64,3.7-64) DO (
+@rem FOR %%V IN (3.5-64,3.6-64,3.7-64) DO (
+FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
 
     py -%%V -m pip install --no-index --find-links=. google-crc32c
     py -%%V -m pip install pytest
