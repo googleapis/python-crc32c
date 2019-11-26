4fb029aca07b43bfeffa9f3b8328d262ef075251
Chris Wilcox <crwilcox@google.com>
2019-11-25 21:15:53 -0800

    fix: add 32 and 64 bit builds

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -25,7 +25,8 @@ git submodule update --init --recursive
 mkdir build
 
 @REM removed -DCRC32C_BUILD_TESTS=no 
-C:\Python37\Scripts\cmake -G %CMAKE_GENERATOR% -DCRC32C_BUILD_BENCHMARKS=no -DBUILD_SHARED_LIBS=yes -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% .
+C:\Python37\Scripts\cmake -G %CMAKE_GENERATOR% -DCRC32C_BUILD_BENCHMARKS=no -DBUILD_SHARED_LIBS=yes ^
+-DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% .
 
 C:\Python37\Scripts\cmake --build . --config RelWithDebInfo --target install
 dir %CRC32C_INSTALL_PREFIX% /b /s
@@ -40,8 +41,9 @@ py -3.7 -m pip install --upgrade pip setuptools wheel
 
 @rem Build wheels (requires CRC32C_INSTALL_PREFIX is set)
 cd %APPVEYOR_BUILD_FOLDER%
-py -3.5 -m pip wheel .
-py -3.6 -m pip wheel .
-py -3.7 -m pip wheel .
-
-@rem TODO: add 64/32 bit both here.
+py -3.5-64 -m pip wheel .
+py -3.5-32 -m pip wheel .
+py -3.6-64 -m pip wheel .
+py -3.6-32 -m pip wheel .
+py -3.7-64 -m pip wheel .
+py -3.7-32 -m pip wheel .
