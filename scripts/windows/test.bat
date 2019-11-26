8a508cac7f01ca167793b8d1e86821ff6d557958
Chris Wilcox <crwilcox@google.com>
2019-11-25 21:55:15 -0800

    fix: temp try just 64 bit builds

--- scripts/windows/test.bat
+++ scripts/windows/test.bat
@@ -14,7 +14,9 @@
 
 
 @rem update python deps and build wheels (requires CRC32C_INSTALL_PREFIX is set)
-FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
+@rem FOR %%V IN (3.5-64,3.5-32,3.6-64,3.6-32,3.7-64,3.7-32) DO (
+FOR %%V IN (3.5-64,3.6-64,3.7-64) DO (
+
     py -%%V -m pip install --no-index --find-links=. google-CRC32C_INSTALL_PREFIX
     py -%%V -m pip install pytest
     py -%%V -m pytest tests
