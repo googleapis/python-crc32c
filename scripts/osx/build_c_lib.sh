fb5e289113ec286178047c8ee0af82e1ca8013be
Chris Wilcox <crwilcox@google.com>
2019-12-26 12:31:29 -0800

    try precreating directory for osx builds

--- scripts/osx/build_c_lib.sh
+++ scripts/osx/build_c_lib.sh
@@ -48,6 +48,7 @@ ${VENV}/bin/python -m pip install "cmake >= 3.12.0"
 # Build `libcrc32c`
 cd ${REPO_ROOT}/crc32c
 mkdir -p build
+mkdir ${CRC32C_INSTALL_PREFIX}
 ${VENV}/bin/cmake \
     -DCMAKE_OSX_ARCHITECTURES="x86_64" \
     -DCRC32C_BUILD_TESTS=no \
