45c30257381c5de0c3a9171b3ae779cb60594753
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2024-05-15 16:11:52 +0000

    remove win py release

--- .kokoro/release-windows-wheel.bat
+++ .kokoro/release-windows-wheel.bat
@@ -33,7 +33,7 @@ set PYTHONUNBUFFERED=1
 @echo "Move into the package, build the distribution and upload."
 set /p TWINE_PASSWORD=<%KOKORO_KEYSTORE_DIR%/73713_google_cloud_pypi_password
 call py -3 setup.py sdist || goto :error
-call py -3 -m twine upload --skip-existing --username gcloudpypi --password "%TWINE_PASSWORD%" dist/* wheels/google_crc32c* || goto :error
+@REM call py -3 -m twine upload --skip-existing --username gcloudpypi --password "%TWINE_PASSWORD%" dist/* wheels/google_crc32c* || goto :error
 
 goto :EOF
 
