e5c040be689aec5c5a40fb3263166779a3db821c
Vishwaraj Anand <vishwaraj.anand00@gmail.com>
2024-05-23 12:41:12 +0000

    added more python versions for 3.11 and 3.12

--- scripts/manylinux/build_on_centos.sh
+++ scripts/manylinux/build_on_centos.sh
@@ -59,6 +59,12 @@ if [[ -z ${BUILD_PYTHON} ]]; then
         elif [[ "${PYTHON_BIN}" == *"310"* ]]; then
             PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
             continue
+        elif [[ "${PYTHON_BIN}" == *"311"* ]]; then
+            PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+            continue
+        elif [[ "${PYTHON_BIN}" == *"312"* ]]; then
+            PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+            continue
         else
             echo "Ignoring unsupported version: ${PYTHON_BIN}"
             echo "====================================="
