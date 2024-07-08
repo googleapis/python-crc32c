0acd26abdc9102209c68ecd27d8690ba27a5f414
Andrew Gorcester <gorcester@google.com>
2024-07-08 16:55:54 -0700

    try to add windows versions to scripts

--- scripts/windows/build.bat
+++ scripts/windows/build.bat
@@ -22,58 +22,60 @@ set CRC32C_INSTALL_PREFIX=%cd%\build\%CONFIGURATION%
 @rem Path to cmake, env var to make it easier to point to a specific version
 set cmake=cmake
 
-@rem python version should be set as an argument, if not, default to python 3.10
-set PYTHON_VERSION=%1
-if "%PYTHON_VERSION%"=="" (
-  echo "Python version was not provided, using Python 3.10"
-  set PYTHON_VERSION=3.10
-)
+@rem Iterate through supported Python versions.
+@rem Historical note: a previous version of this script accepted the version as
+@rem an argument.
+FOR %%P IN ("3.8", "3.9", "3.10", "3.11", "3.12") DO (
 
-py -0
-py -%PYTHON_VERSION% -m pip install cmake
+    echo "Listing Python versions"
+    py -0
 
-@rem Add directory as safe to avoid "detected dubious ownership" fatal issue
-git config --global --add safe.directory C:/tmpfs/src/github/python-crc32c
-git submodule update --init --recursive
+    echo "Installing cmake for Python %%P"
+    py -%%P -m pip install cmake
 
-FOR %%V IN (32,64) DO (
-    set TARGET_PLATFORM="x64"
+    @rem Add directory as safe to avoid "detected dubious ownership" fatal issue
+    git config --global --add safe.directory C:/tmpfs/src/github/python-crc32c
+    git submodule update --init --recursive
 
-    if "%%V"=="32" (
-        set TARGET_PLATFORM="Win32"
+    FOR %%V IN (32,64) DO (
+        set TARGET_PLATFORM="x64"
+
+        if "%%V"=="32" (
+            set TARGET_PLATFORM="Win32"
+        )
+        echo "Target Platform: !TARGET_PLATFORM!"
+
+        git config --global --add safe.directory C:/tmpfs/src/github/python-crc32c/google_crc32c
+        pushd google_crc32c
+        @rem reset hard to cleanup any changes done by a previous build.
+        git reset --hard
+        git clean -fxd
+
+        del /s /q CMakeFiles\
+        del CMakeCache.txt
+
+        mkdir build
+        cd build
+
+        echo "Running cmake with Generator:  %CMAKE_GENERATOR%, Platform: !TARGET_PLATFORM!, Install Prefix: %CRC32C_INSTALL_PREFIX%"
+
+        %cmake% -G %CMAKE_GENERATOR% -A !TARGET_PLATFORM! -DCRC32C_BUILD_BENCHMARKS=no -DCRC32C_BUILD_TESTS=no -DBUILD_SHARED_LIBS=yes -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes -DCRC32C_USE_GLOG=0 -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% ..
+
+        %cmake% --build . --config "%CONFIGURATION%" --target install
+
+        dir %CRC32C_INSTALL_PREFIX% /b /s
+        popd
+
+        dir  %CRC32C_INSTALL_PREFIX%\bin
+        echo "Copying Binary to root: %CRC32C_INSTALL_PREFIX%\bin\crc32c.dll"
+        copy %CRC32C_INSTALL_PREFIX%\bin\crc32c.dll .
+
+        py -%%P-%%V -m pip install --upgrade pip setuptools wheel
+        echo "Building C extension"
+        py -%%P-%%V setup.py build_ext -v --include-dirs=%CRC32C_INSTALL_PREFIX%\include --library-dirs=%CRC32C_INSTALL_PREFIX%\lib
+        echo "Building Wheel"
+        py -%%P-%%V -m pip wheel . --wheel-dir wheels/
     )
-    echo "Target Platform: !TARGET_PLATFORM!"
-
-    git config --global --add safe.directory C:/tmpfs/src/github/python-crc32c/google_crc32c
-    pushd google_crc32c
-    @rem reset hard to cleanup any changes done by a previous build.
-    git reset --hard
-    git clean -fxd
-
-    del /s /q CMakeFiles\
-    del CMakeCache.txt
-
-    mkdir build
-    cd build
-
-    echo "Running cmake with Generator:  %CMAKE_GENERATOR%, Platform: !TARGET_PLATFORM!, Install Prefix: %CRC32C_INSTALL_PREFIX%"
-
-    %cmake% -G %CMAKE_GENERATOR% -A !TARGET_PLATFORM! -DCRC32C_BUILD_BENCHMARKS=no -DCRC32C_BUILD_TESTS=no -DBUILD_SHARED_LIBS=yes -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=yes -DCRC32C_USE_GLOG=0 -DCMAKE_INSTALL_PREFIX:PATH=%CRC32C_INSTALL_PREFIX% ..
-
-    %cmake% --build . --config "%CONFIGURATION%" --target install
-
-    dir %CRC32C_INSTALL_PREFIX% /b /s
-    popd
-
-    dir  %CRC32C_INSTALL_PREFIX%\bin
-    echo "Copying Binary to root: %CRC32C_INSTALL_PREFIX%\bin\crc32c.dll"
-    copy %CRC32C_INSTALL_PREFIX%\bin\crc32c.dll .
-
-    py -%PYTHON_VERSION%-%%V -m pip install --upgrade pip setuptools wheel
-    echo "Building C extension"
-    py -%PYTHON_VERSION%-%%V setup.py build_ext -v --include-dirs=%CRC32C_INSTALL_PREFIX%\include --library-dirs=%CRC32C_INSTALL_PREFIX%\lib
-    echo "Building Wheel"
-    py -%PYTHON_VERSION%-%%V -m pip wheel . --wheel-dir wheels/
 )
 
 echo "Windows build has completed successfully"
@@ -84,5 +86,5 @@ set "GOOGLE_APPLICATION_CREDENTIALS=%KOKORO_GFILE_DIR%\service-account.json"
 dir "%cd%\wheels"
 call gcloud auth activate-service-account --key-file=%GOOGLE_APPLICATION_CREDENTIALS%
 echo "Windows wheels uploading ..."
-gsutil cp "%cd%\wheels\*" gs://python_crc32c/win_wheels/
+gsutil cp "%cd%\wheels\*" gs://python_crc32c/
 echo "Windows wheels uploaded successfully"
--- scripts/windows/test.bat
+++ scripts/windows/test.bat
@@ -11,19 +11,14 @@
 @rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 @rem See the License for the specific language governing permissions and
 @rem limitations under the License.
-set PYTHON_VERSION=%1
-if "%PYTHON_VERSION%"=="" (
-  echo "Python version was not provided, using Python 3.10"
-  set PYTHON_VERSION=3.10
-)
 
-@rem update python deps and build wheels (requires CRC32C_INSTALL_PREFIX is set)
-@REM FOR %%V IN (3.9-64,3.9-32) DO (
-FOR %%V IN (%PYTHON_VERSION%-32, %PYTHON_VERSION%-64) DO (
-    py -%%V -m pip install --no-index --find-links=wheels google-crc32c --force-reinstall
+FOR %%P IN ("3.8", "3.9", "3.10", "3.11", "3.12") DO (
+    FOR %%V IN (%%P-32, %%P-64) DO (
+        py -%%V -m pip install --no-index --find-links=wheels google-crc32c --force-reinstall
 
-    py -%%V ./scripts/check_crc32c_extension.py
+        py -%%V ./scripts/check_crc32c_extension.py
 
-    py -%%V -m pip install pytest
-    py -%%V -m pytest tests
+        py -%%V -m pip install pytest
+        py -%%V -m pytest tests
+    )
 )
