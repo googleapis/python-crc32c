77d846f057e527bcd9587a6dc228ef5fee771290
Andrew Gorcester <gorcester@google.com>
2024-07-08 16:27:01 -0700

    remove copy_dll early return and readd debugging steps

--- scripts/manylinux/build.sh
+++ scripts/manylinux/build.sh
@@ -60,8 +60,8 @@ echo "Build completed"
 
 # Upload wheels to GCS for debugging. Uncomment only when needed.
 
-# export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
-# gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
-# ls ${REPO_ROOT}/wheels/
-# gsutil cp ${REPO_ROOT}/wheels/* gs://python_crc32c/
-# echo "Linux wheels uploaded successfully"
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+ls ${REPO_ROOT}/wheels/
+gsutil cp ${REPO_ROOT}/wheels/* gs://python_crc32c/
+echo "Linux wheels uploaded successfully"
