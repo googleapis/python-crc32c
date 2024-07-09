3b84d1972204b0a7a80501bdabe7b8769368fc05
Anthonios Partheniou <partheniou@google.com>
2024-07-09 23:03:34 +0000

    fix No such file or directory error

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -64,7 +64,7 @@ echo "## Uploading Wheels ##"
 # TODO: ONE OF THE BELOW WORKS
 # Move into the package, build the distribution and upload.
 TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-1")
-cd github/python-crc32c
+cd ${REPO_ROOT}
 python3 setup.py sdist bdist_wheel
 #twine upload --username __token__ --password "${TWINE_PASSWORD}" dist/*
 
