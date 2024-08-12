d1b4e4688d54463f7293bc9648024129ba9204c7
Anthonios Partheniou <partheniou@google.com>
2024-08-12 21:13:25 +0000

    publish wheels for all runtimes

--- .kokoro/release.sh
+++ .kokoro/release.sh
@@ -16,61 +16,84 @@
 set -eo pipefail
 echo "Release script started"
 
-if [ "$(uname)" == "Darwin" ]; then
-    # Mac OS
-    PYTHON=$(PYENV_VERSION=3.9 pyenv which python)
-    PYTHON_BIN=$(dirname ${PYTHON})
+# Collect all target Python versions.
+for PYTHON_BIN in /opt/python/*/bin; do
+    # H/T: https://stackoverflow.com/a/229606/1068170
+    if [[ "${PYTHON_BIN}" == *"38"* ]]; then
+        PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+        continue
+    elif [[ "${PYTHON_BIN}" == *"39"* ]]; then
+        PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+        continue
+    elif [[ "${PYTHON_BIN}" == *"310"* ]]; then
+        PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+        continue
+    elif [[ "${PYTHON_BIN}" == *"311"* ]]; then
+        PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+        continue
+    elif [[ "${PYTHON_BIN}" == *"312"* ]]; then
+        PYTHON_VERSIONS="${PYTHON_VERSIONS} ${PYTHON_BIN}"
+        continue
+    else
+        echo "Ignoring unsupported version: ${PYTHON_BIN}"
+        echo "====================================="
+    fi
+done
 
-    RELEASETOOL=${PYTHON_BIN}/releasetool
-    TWINE=${PYTHON_BIN}/twine
-    ${PYTHON} -m pip install gcp-releasetool twine --user
+for PYTHON_BIN in ${PYTHON_VERSIONS}; do
 
-    echo "Change to code directory"
-    REPO_ROOT=$(pwd)
-    cd "${REPO_ROOT}"
-    ls
+    if [ "$(uname)" == "Darwin" ]; then
+        RELEASETOOL=${PYTHON_BIN}/releasetool
+        TWINE=${PYTHON_BIN}/twine
+        ${PYTHON} -m pip install gcp-releasetool twine --user
 
-else
-    # Kokoro Linux
-    mv /keys/73713_google-cloud-pypi-token-keystore-1 /73713_google-cloud-pypi-token-keystore-1
+        echo "Change to code directory"
+        REPO_ROOT=$(pwd)
+        cd "${REPO_ROOT}"
+        ls
 
-    PATH=/opt/python/cp39-cp39/bin/:$PATH
-    PYTHON_BIN=/opt/python/cp39-cp39/bin/
-    RELEASETOOL=${PYTHON_BIN}/releasetool
-    PYTHON=${PYTHON_BIN}/python
-    TWINE=${PYTHON_BIN}/twine
-    ${PYTHON} -m pip install gcp-releasetool twine
+    else
+        # Kokoro Linux
+        mv /keys/73713_google-cloud-pypi-token-keystore-1 /73713_google-cloud-pypi-token-keystore-1
 
-    echo "Change to code directory"
-    REPO_ROOT=/var/code/python-crc32c/
-    cd "${REPO_ROOT}"
-    ls
+        PATH=$PYTHON_BIN:$PATH
+        RELEASETOOL=${PYTHON_BIN}/releasetool
+        PYTHON=${PYTHON_BIN}/python
+        TWINE=${PYTHON_BIN}/twine
+        ${PYTHON} -m pip install gcp-releasetool twine
 
-fi
+        echo "Change to code directory"
+        REPO_ROOT=/var/code/python-crc32c/
+        cd "${REPO_ROOT}"
+        ls
 
-echo "Download dependencies for release script"
+    fi
 
-# Start the releasetool reporter
-${PYTHON} -m pip install --require-hashes -r ${REPO_ROOT}/.kokoro/requirements.txt
-${RELEASETOOL} publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
+    echo "Download dependencies for release script"
 
-# Ensure that we have the latest versions of Twine, Wheel, and Setuptools.
-${PYTHON} -m pip install --upgrade twine wheel setuptools --user
+    # Start the releasetool reporter
+    ${PYTHON} -m pip install --require-hashes -r ${REPO_ROOT}/.kokoro/requirements.txt
+    ${RELEASETOOL} publish-reporter-script > /tmp/publisher-script; source /tmp/publisher-script
 
-# Disable buffering, so that the logs stream through.
-export PYTHONUNBUFFERED=1
-echo "## RELASE WORKFLOW SUCCESSFUL ##"
-echo "## Uploading Wheels ##"
-# TODO: ONE OF THE BELOW WORKS
-# Move into the package, build the distribution and upload.
-TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-1")
-cd ${REPO_ROOT}
-${PYTHON} setup.py sdist bdist_wheel
-#${TWINE} upload --username __token__ --password "${TWINE_PASSWORD}" dist/*
+    # Ensure that we have the latest versions of Twine, Wheel, and Setuptools.
+    ${PYTHON} -m pip install --upgrade twine wheel setuptools --user
 
-${PYTHON} setup.py sdist
-# Remove *-linux_x86_64.whl wheels which cannot be pushed to PyPI.
-# Other we get, `Binary wheel has an unsupported platform tag 'linux_x86_64'`.
-rm -rf wheels/*-linux_x86_64.whl
-echo "Skipping *-linux_x86_64.whl wheels"
-#${TWINE} upload --skip-existing --username gcloudpypi --password "${TWINE_PASSWORD}" dist/* wheels/*
+    # Disable buffering, so that the logs stream through.
+    export PYTHONUNBUFFERED=1
+    echo "## RELASE WORKFLOW SUCCESSFUL ##"
+    echo "## Uploading Wheels ##"
+    # TODO: ONE OF THE BELOW WORKS
+    # Move into the package, build the distribution and upload.
+    TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-1")
+    cd ${REPO_ROOT}
+    # Remove *-linux_x86_64.whl wheels which cannot be pushed to PyPI.
+    # Other we get `Binary wheel has an unsupported platform tag 'linux_x86_64'`.
+    rm -rf wheels/*-linux_x86_64.whl
+    echo "Skipping *-linux_x86_64.whl wheels"
+    ${PYTHON} setup.py sdist bdist_wheel
+    #${TWINE} upload --username __token__ --password "${TWINE_PASSWORD}" dist/*
+
+    ${PYTHON} setup.py sdist
+    #${TWINE} upload --skip-existing --username gcloudpypi --password "${TWINE_PASSWORD}" dist/* wheels/*
+
+done
