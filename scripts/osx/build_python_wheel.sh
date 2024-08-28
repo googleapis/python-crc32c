e6e1677e9074c0705c496de4696c60be62ebe9f8
Anthonios Partheniou <partheniou@google.com>
2024-08-28 12:44:03 +0000

    upload wheels to gcs

--- scripts/osx/build_python_wheel.sh
+++ scripts/osx/build_python_wheel.sh
@@ -78,6 +78,12 @@ ${VENV}/bin/delocate-wheel \
     --check-archs \
     ${DIST_WHEELS}/google_crc32c*${PY_TAG}*.whl
 
+export GOOGLE_APPLICATION_CREDENTIALS=${KOKORO_GFILE_DIR}/service-account.json
+gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
+ls ${REPO_ROOT}/wheels/
+gsutil cp ${REPO_ROOT}/wheels/* gs://python_crc32c/
+echo "Osx wheels uploaded successfully"
+
 if [[ "${PUBLISH_WHEELS}" == "true" ]]; then
     . /${OSX_DIR}/publish_python_wheel.sh
 fi
