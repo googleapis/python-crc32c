da2e62c4408eadbd66ad2034c17e348771f467e7
Andrew Gorcester <gorcester@google.com>
2025-03-13 13:44:42 -0700

    continue iterating on build scripts

--- scripts/osx/check.sh
+++ scripts/osx/check.sh
@@ -19,8 +19,8 @@ VERSION=$(awk "/version \= ([0-9.]+)/" setup.cfg)
 PACKAGE_VERSION=${VERSION:10}
 
 # ``readlink -f`` is not our friend on OS X. This relies on **some**
-# ``python`` being installed.
-SCRIPT_FI=$(python -c "import os; print(os.path.realpath('${0}'))")
+# ``python3`` being installed.
+SCRIPT_FI=$(python3 -c "import os; print(os.path.realpath('${0}'))")
 OSX_DIR=$(dirname ${SCRIPT_FI})
 SCRIPTS_DIR=$(dirname ${OSX_DIR})
 export REPO_ROOT=$(dirname ${SCRIPTS_DIR})
