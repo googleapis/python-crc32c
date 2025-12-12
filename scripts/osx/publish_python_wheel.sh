5c5ef372ea220660dedcbe033835dbf4cfccca3c
Daniel Sanche <d.sanche14@gmail.com>
2025-12-11 16:22:13 -0800

    fixed twine call

--- scripts/osx/publish_python_wheel.sh
+++ scripts/osx/publish_python_wheel.sh
@@ -27,4 +27,4 @@ python -m releasetool publish-reporter-script > /tmp/publisher-script; source /t
 # Disable logging
 set +x
 TWINE_PASSWORD=$(cat "${KOKORO_KEYSTORE_DIR}/73713_google-cloud-pypi-token-keystore-3")
-${PYTHON} -m twine upload --skip-existing --username __token__ --password "${TWINE_PASSWORD}" ${REPO_ROOT}/wheels/*
+python -m twine upload --skip-existing --username __token__ --password "${TWINE_PASSWORD}" ${REPO_ROOT}/wheels/*
