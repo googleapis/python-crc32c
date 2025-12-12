618e550750ac4eaf612d59fb727e75a963cd6191
Daniel Sanche <d.sanche14@gmail.com>
2025-12-11 19:30:00 -0800

    remove pyenv update

--- scripts/osx/build.sh
+++ scripts/osx/build.sh
@@ -41,7 +41,6 @@ ${OSX_DIR}/build_c_lib.sh
 
 SUPPORTED_PYTHON_VERSIONS=("3.9" "3.10" "3.11" "3.12" "3.13", "3.14")
 
-pyenv update
 pyenv install --list
 for PYTHON_VERSION in ${SUPPORTED_PYTHON_VERSIONS[@]}; do
     if [ -z "$(pyenv versions --bare | grep $PYTHON_VERSION)" ]; then
